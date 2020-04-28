5d93dab076a890e2e14ffd1d8f18b830539dbdcc
TornikeJ <t.janashia11@gmail.com>
2020-04-28 14:44:15 +0400

    start mvc

---
+++ EmployeeManagment/EmployeeManagment/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using EmployeeManagment.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EmployeeManagment.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly IEmployeeRepository employeeRepository;
+
+        public HomeController(IEmployeeRepository employeeRepository)
+        {
+            this.employeeRepository = employeeRepository;
+        }
+        public string Index()
+        {
+            return employeeRepository.GetEmployee(1).Name;
+
+        }
+
+        public ViewResult Details()
+        {
+            Employee model = employeeRepository.GetEmployee(1);
+            return View(model);
+        }
+    }
+}
--- EmployeeManagment/EmployeeManagment/Startup.cs
+++ EmployeeManagment/EmployeeManagment/Startup.cs
@@ -2,19 +2,31 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using EmployeeManagment.Models;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace EmployeeManagment
 {
     public class Startup
     {
+        private readonly IConfiguration config;
+
+        public Startup(IConfiguration config)
+        {
+            this.config = config;
+        }
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc().AddXmlDataContractSerializerFormatters();
+
+            services.AddSingleton<IEmployeeRepository, MockEmployeeRepository>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -25,9 +37,12 @@ namespace EmployeeManagment
                 app.UseDeveloperExceptionPage();
             }
 
+            app.UseStaticFiles();
+            app.UseMvcWithDefaultRoute();
+
             app.Run(async (context) =>
             {
-                await context.Response.WriteAsync("Hello World!");
+                await context.Response.WriteAsync(config["MyKey"]);
             });
         }
     }
